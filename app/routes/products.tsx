34f5abb446ca081839dbe6303f8568bd46e7e8b2
Mark Deutsch <markpd@gmail.com>
2022-03-24 17:09:09 -0400

    Added search

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,28 +1,51 @@
-import { json, useLoaderData, Link } from 'remix';
+import { json, useLoaderData, Link, Form, useSearchParams } from 'remix';
 import type { LoaderFunction } from 'remix';
+import {
+  Button,
+  Box,
+  Typography,
+  TextField,
+  IconButton,
+  InputAdornment,
+  Alert,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
 import ctClient from '~/helpers/ctClient';
 import { ProductList } from '~/components/ProductList';
 import { getErrorMessage } from '~/utils';
-import { Button, Box, Typography } from '@mui/material';
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const offset = url.searchParams.get('offset') ?? '0';
 
-  const response = await ctClient
-    .graphql()
-    .post({
-      body: {
-        query: `
+  const params = ['staged:false', 'limit:20', `offset:${offset}`];
+
+  const search = url.searchParams.get('search');
+  if (search) {
+    params.push(`text:"${search}"`);
+    params.push('locale:"en"');
+  } else {
+    params.push('sorts:"name.en asc"');
+  }
+
+  let response;
+
+  try {
+    response = await ctClient
+      .graphql()
+      .post({
+        body: {
+          query: `
         query {
-          productProjectionSearch(staged:false, offset:${offset} limit:20, sorts:"name.en asc") {
+          productProjectionSearch(${params.join(', ')}) {
             offset,
             count,
             total,
             results {
               id, 
               key,
-              name(locale: "en"),
+              name(locale:"en"),
               masterVariant {
                 images {
                   url
@@ -31,55 +54,94 @@ export const loader: LoaderFunction = async ({ request }) => {
             }  
           }
         }`,
-      },
-    })
-    .execute()
-    .catch((error: unknown) => {
-      console.error(getErrorMessage(error));
-    });
+        },
+      })
+      .execute();
+  } catch (error: any) {
+    // I wish I didn't cast this to `any` but I can't figure out a better solution
+    // for handling when I _know_ that it's a GraphQL response containing errors.
+    let errors = [];
+    if (error?.body?.errors) {
+      errors = error?.body?.errors.map((e: any) => e.message);
+    } else {
+      errors.push(`Error fetching products: ${getErrorMessage(error)}`);
+    }
 
-  const data = response.body.data?.productProjectionSearch;
+    return json({ errors, results: [], offset: 0, count: 0, total: 0 });
+  }
 
-  return json(data);
+  if (response?.body?.data) {
+    return json(response.body.data.productProjectionSearch);
+  }
+
+  // base case
+  return json({ results: [], offset: 0, count: 0, total: 0 });
 };
 
-export default function Products({ pathname = '/products' }) {
-  const { results: products, offset, count, total } = useLoaderData();
+export default function Products() {
+  const { errors, results: products, offset, total, count } = useLoaderData();
 
-  const urlNext = `${pathname}?offset=${offset + 20}`;
-  const urlPrev = `${pathname}?offset=${offset - 20}`;
+  const [searchParams] = useSearchParams();
+  searchParams.set('offset', String(offset + 20));
+  const urlNext = `?${searchParams.toString()}`;
+  searchParams.set('offset', String(offset - 20));
+  const urlPrev = `?${searchParams.toString()}`;
 
   return (
     <>
       <Typography variant="h2">Products</Typography>
-      <Typography style={{ textAlign: 'center' }} variant="h6">
-        Showing {offset} - {offset + count} / {total}
-      </Typography>
+      {Array.isArray(errors) &&
+        errors.map(error => (
+          <Alert key={error} severity="error">
+            {error}
+          </Alert>
+        ))}
       <Box style={{ textAlign: 'center' }}>
-        <Button
-          component={Link}
-          disabled={offset === 0}
-          prefetch="intent"
-          style={{ margin: '0 8px', minWidth: 200 }}
-          to={urlPrev}
-          type="button"
-        >
-          Show Previous
-        </Button>
-        <Button
-          component={Link}
-          disabled={offset > total - count}
-          prefetch="intent"
-          style={{ margin: '0 8px', minWidth: 200 }}
-          to={urlNext}
-          type="button"
-        >
-          Show Next
-        </Button>
+        <Form method="get">
+          <TextField
+            defaultValue={searchParams.get('search') ?? undefined}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton type="submit">
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            label="Search"
+            margin="normal"
+            name="search"
+            type="search"
+          />
+        </Form>
+        <Typography variant="h6">
+          Showing {offset} - {offset + count} / {total}
+        </Typography>
+        <Box>
+          <Button
+            component={Link}
+            disabled={offset === 0}
+            prefetch="intent"
+            style={{ margin: '0 8px', minWidth: 200 }}
+            to={urlPrev}
+            type="button"
+          >
+            Show Previous
+          </Button>
+          <Button
+            component={Link}
+            disabled={offset + 20 > total - count}
+            prefetch="intent"
+            style={{ margin: '0 8px', minWidth: 200 }}
+            to={urlNext}
+            type="button"
+          >
+            Show Next
+          </Button>
+        </Box>
       </Box>
-      <div>
-        <ProductList products={products} />
-      </div>
+      <ProductList products={products} />
     </>
   );
 }
